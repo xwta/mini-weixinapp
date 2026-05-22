3ae0375d5c978cb95aa83f99224ad4ca5c6149ad
xwta <110807199+xwta@users.noreply.github.com>
2026-05-23 02:19:28 +0800

    backend: export like and follow models

--- apps/server/app/models/__init__.py
+++ apps/server/app/models/__init__.py
@@ -1,8 +1,10 @@
 from app.models.ai_generation_log import AiGenerationLog
 from app.models.favorite import Favorite
+from app.models.follow import Follow
+from app.models.like import Like
 from app.models.order import Order
 from app.models.practice_record import PracticeRecord
 from app.models.song import Song
 from app.models.user import User
 
-__all__ = ['AiGenerationLog', 'Favorite', 'Order', 'PracticeRecord', 'Song', 'User']
+__all__ = ['AiGenerationLog', 'Favorite', 'Follow', 'Like', 'Order', 'PracticeRecord', 'Song', 'User']
