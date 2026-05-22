e20dc2c82942a50f0959c9d378229ed37113a5d5
xwta <110807199+xwta@users.noreply.github.com>
2026-05-23 01:49:51 +0800

    frontend: add root app component

---
+++ apps/miniprogram/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+import { useAuthStore } from './stores/auth'
+
+onLaunch(() => {
+  const auth = useAuthStore()
+  auth.restore()
+})
+
+onShow(() => {})
+onHide(() => {})
+</script>
+
+<style lang="scss">
+page {
+  min-height: 100%;
+  background: #fafaf6;
+  color: #1f2428;
+  font-family: -apple-system, BlinkMacSystemFont, 'PingFang SC', 'Microsoft YaHei', sans-serif;
+}
+
+view,
+text,
+button,
+input,
+textarea {
+  box-sizing: border-box;
+}
+
+button {
+  margin: 0;
+  padding: 0;
+  background: transparent;
+  border: none;
+  line-height: 1;
+}
+
+button::after {
+  border: none;
+}
+</style>
