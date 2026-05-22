43122d23ed0a67b513eb4f37ded5d6e0dfbc5eb8
xwta <110807199+xwta@users.noreply.github.com>
2026-05-23 02:07:59 +0800

    backend: export practice record model

--- apps/server/app/models/__init__.py
+++ apps/server/app/models/__init__.py
@@ -1,7 +1,8 @@
 from app.models.ai_generation_log import AiGenerationLog
 from app.models.favorite import Favorite
 from app.models.order import Order
+from app.models.practice_record import PracticeRecord
 from app.models.song import Song
 from app.models.user import User
 
-__all__ = ['AiGenerationLog', 'Favorite', 'Order', 'Song', 'User']
+__all__ = ['AiGenerationLog', 'Favorite', 'Order', 'PracticeRecord', 'Song', 'User']
